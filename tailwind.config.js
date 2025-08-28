91d7c1b2bcdd2e3129eb86d93beab57a0420ebf4
Trung Lam <trungltgcs16028@fpt.edu.vn>
2025-08-28 17:56:46 +0700

    change file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
+/** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{js,jsx}'],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#111827',
+        secondary: '#1f2937',
+        accent: '#f3f4f6',
+      },
+    },
   },
   plugins: [],
 }
